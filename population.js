3235fd741e99f22a8db4f775e20e0b958a99a910
PolliHam <polina.shenets@gmail.com>
2018-11-23 04:12:41 +0300

    Task 1.4

--- population.js
+++ population.js
@@ -62,3 +62,56 @@ Promise.any(anyPromises).then((res) => {
 });
 
 
+//TASK 1.4
+//Возвращает обещание, которое выполняется, когда выполняются все свойства объекта или значения «карты».
+Promise.props({
+    greeceM: axios.get(`http://api.population.io:80/1.0/mortality-distribution/Greece/male/0/today/`),
+    turkeyM: axios.get(`http://api.population.io:80/1.0/mortality-distribution/Turkey/male/0/today/`)
+    greeceF: axios.get(`http://api.population.io:80/1.0/mortality-distribution/Greece/female/0/today/`),
+    turkeyF: axios.get(`http://api.population.io:80/1.0/mortality-distribution/Turkey/female/0/today/`)
+}).then((result) => {
+    console.log("\tTASK 1.4\t   Greece, Turkey");
+
+    let ageGM = 0;
+    let mpGM = -100;
+    result.greeceM.data["mortality_distribution"].forEach(element=>{
+        if(element["mortality_percent"] > mpGM){
+            mpGM =element["mortality_percent"];
+            ageGM = element["age"];
+        }
+    });
+    console.log(`Возраст наибольшей смертности мужчин Греции: ${ageGM}`);
+
+    let ageGF = 0;
+    let mpGF = -100;
+    result.greeceF.data["mortality_distribution"].forEach(element=>{
+        if(element["mortality_percent"] > mpGF){
+            mpGF =element["mortality_percent"];
+            ageGF = element["age"];
+        }
+    });
+    console.log(`Возраст наибольшей смертности женщин Греции: ${ageGF}`);
+
+    let ageTM = 0;
+    let mpTM = -100;
+    result.turkeyM.data["mortality_distribution"].forEach(element=>{
+        if(element["mortality_percent"] > mpTM){
+            mpTM =element["mortality_percent"];
+            ageTM = element["age"];
+        }
+    });
+    console.log(`Возраст наибольшей смертности мужчин Турции: ${ageTM}`);
+
+    let ageTF = 0;
+    let mpTF = -100;
+    result.turkeyF.data["mortality_distribution"].forEach(element=>{
+        if(element["mortality_percent"] > mpTF){
+            mpTF =element["mortality_percent"];
+            ageTF = element["age"];
+        }
+    });
+    console.log(`Возраст наибольшей смертности женщин Турции: ${ageTF}`);
+}).catch((err) => {
+    console.log(`Error TASK 1.4 :${err}`);
+});
+
